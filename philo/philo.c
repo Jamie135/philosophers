101c02c9011bb491afa7b75e875dc42ace09b50a
unknown <jamie.gworld@hotmail.com>
2023-02-22 16:55:29 +0700

    OK

---
+++ philo/philo.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbureera <pbureera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 22:47:35 by pbureera          #+#    #+#             */
+/*   Updated: 2023/02/10 15:41:51 by pbureera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	valid_args(int argc, char **argv)
+{
+	size_t	i;
+
+	if (argc < 5 || argc > 6)
+	{
+		ft_check_args();
+		return (1);
+	}
+	i = 1;
+	while (argv[i])
+	{
+		if (!ft_is_digit(argv[i]) || (!ft_atoi(argv[i])))
+		{
+			printf("Invalid argument\n");
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_arg	args;
+
+	if (valid_args(argc, argv))
+		return (1);
+	if (ft_init_args(&args, argc, argv) == 1)
+		return (1);
+	ft_init_mutex(&args);
+	ft_init_philosophers(&args);
+	ft_init_threads(&args);
+	ft_end_threads(&args);
+	unlock_and_destroy_mutex(&args);
+	free_all(&args);
+	return (0);
+}
