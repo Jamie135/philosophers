b70b239c700c07471590150ad4326d4e97f18a3f
unknown <jamie.gworld@hotmail.com>
2023-02-25 23:12:43 +0700

    OK

--- philo/actions.c
+++ philo/actions.c
@@ -31,6 +31,27 @@ void	eat(t_philo *philo)
 	pthread_mutex_unlock(&philo->data->meal_mutex);
 }
 
+void	eat_while_philo_need(t_philo *philo)
+{
+	while (is_alive(philo) == 1 && philo->nb_meal > 0)
+	{		
+		eat(philo);
+		if (philo->nb_meal > 0)
+			sleep_and_think(philo);
+	}
+	pthread_mutex_lock(&philo->data->meal_mutex);
+	philo->data->nb_time_must_eat--;
+	pthread_mutex_unlock(&philo->data->meal_mutex);
+}
+
+void	one_philo_eat(t_philo *philo)
+{
+	display(philo, "has taken a fork");
+	usleep(philo->data->time_to_die * 1000);
+	printf("%ldms %d died\n", (get_time() - \
+	philo->data->start_time), philo->id);
+}
+
 void	sleep_and_think(t_philo *philo)
 {
 	if (is_alive(philo) == 0)
